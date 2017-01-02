276411a3563279dd54bf0a4c32e4c13e407a1d51
Yuwen <294958213@qq.com>
2017-01-02 11:54:41 +0800

    Mon, 02 Jan 2017 11:54:41 CST

---
+++ upload_commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#By Yuwen 20170102 upload_commit
+#Version 1.0
+HOST_OS=$( uname )
+git stage gfw_whitelist.txt
+if [ "$HOST_OS" = "Linux" ] || [[ "$HOST_OS" =~ "MINGW64_NT" ]] || [[ "$HOST_OS" =~ "MINGW32_NT" ]]; then
+    DATE="$(echo $(date --rfc-2822)|sed -n -e 's/\+0800/CST/p')"
+elif [ "$HOST_OS" = "Darwin" ]; then
+    DATE="$(echo $(date +'%a, %d %b %Y %T CST'))"
+fi
+git commit -a -m "$DATE"
+git push
